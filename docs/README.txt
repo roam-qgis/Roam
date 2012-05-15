f88caf13e3da765e7838f458f82d3560dc612e6c
Nathan Woodrow <woodrow.nathan@gmail.com>
2012-05-15 16:44:52 +1000

    Add start of docs

---
+++ docs/README.txt
@@ -0,0 +1,18 @@
+====================
+QGIS Data Collection
+====================
+
+Install
+----------
+
+Conventions
+-----------
+QGIS Data collection follows a convention over configuration style.
+
+Layer field names map to object names in Qt form (.ui)
+
+Images saved from drawing pad are stored in data\{layername}\images. Images have
+the following convetions
+
+    {id}_{fieldname}.jpg
+
---
+++ docs/rstpep2html.py
@@ -0,0 +1,25 @@
+#!C:\OSGeo4W\bin\python.exe
+
+# $Id$
+# Author: David Goodger <goodger@python.org>
+# Copyright: This module has been placed in the public domain.
+
+"""
+A minimal front end to the Docutils Publisher, producing HTML from PEP
+(Python Enhancement Proposal) documents.
+"""
+
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+
+
+description = ('Generates (X)HTML from reStructuredText-format PEP files.  '
+               + default_description)
+
+publish_cmdline(reader_name='pep', writer_name='pep_html',
+                description=description)
