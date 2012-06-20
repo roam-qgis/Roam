e6cf4a82678e86bc29f2fea2f45ab3fb3af1f6b2
Nathan Woodrow <woodrow.nathan@gmail.com>
2012-06-20 14:16:29 +1000

    Fix windows hook to handle destroy call

--- power.py
+++ power.py
@@ -6,6 +6,7 @@ import win32api
 from PyQt4.QtGui import *
 from PyQt4.QtCore import QObject, pyqtSignal
 import sys
+from utils import log
 
 class PowerState(QObject):
     poweroff = pyqtSignal()
@@ -18,10 +19,17 @@ class PowerState(QObject):
                                                 self.__WndProc)
 
     def __WndProc(self, hWnd, msg, wParam, lParam):
+        if msg == win32con.WM_DESTROY:
+            win32api.SetWindowLong(self.widget.winId(),
+                                    win32con.GWL_WNDPROC,
+                                    self.oldWndProc)
+                                    
         if msg == win32con.WM_POWERBROADCAST:
             if wParam == win32con.PBT_APMSUSPEND:
+                log("Power off")
                 self.poweroff.emit()
-            elif wParam == win32con.PBT_APMRESUMEAUTOMATIC:
+            elif wParam == win32con.PBT_APMRESUMESUSPEND:
+                log("Power ON")
                 self.poweron.emit()
 
         return win32gui.CallWindowProc(self.__oldProc,hWnd, msg, wParam, lParam)
