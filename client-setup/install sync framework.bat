fe15c43e1a7896b1661a11c0c9557e34bb2bb4f5
Nathan Woodrow <woodrow.nathan@gmail.com>
2012-07-12 08:29:09 +1000

    Add client setup files

---
+++ client-setup/install sync framework.bat
@@ -0,0 +1,3 @@
+msiexec /i /passive "Sync Framework 2.1\DatabaseProviders-v3.1-x86-ENU.msi"
+msiexec /i /passive "ProviderServices-v2.1-x86-ENU.msi"
+msiexec /i /passive "Synchronization-v2.1-x86-ENU.msi"
