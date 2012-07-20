afc006dc0ada04d2f7a4deb0ea8cefdf9def96a1
Nathan Woodrow <woodrow.nathan@gmail.com>
2012-07-20 10:17:07 +1000

    Update sync instaler scripts

--- client-setup/install sync framework.bat
+++ client-setup/install sync framework.bat
@@ -1,3 +1,3 @@
 msiexec /i /passive "Sync Framework 2.1\DatabaseProviders-v3.1-x86-ENU.msi"
-msiexec /i /passive "ProviderServices-v2.1-x86-ENU.msi"
-msiexec /i /passive "Synchronization-v2.1-x86-ENU.msi"
+msiexec /i /passive "Sync Framework 2.1\ProviderServices-v2.1-x86-ENU.msi"
+msiexec /i /passive "Sync Framework 2.1\Synchronization-v2.1-x86-ENU.msi"
