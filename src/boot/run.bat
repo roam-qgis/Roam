8ee760abcfab03831ede4825afef4c55ee338ead
Nathan Woodrow <woodrow.nathan@gmail.com>
2012-09-10 10:41:57 +1000

    Remove reg hack

--- src/boot/run.bat
+++ src/boot/run.bat
@@ -1,5 +1,2 @@
 @ECHO OFF
-REGEDIT /e qgis-customization.reg "HKEY_CURRENT_USER\Software\QuantumGIS\QGISCUSTOMIZATION"
-REM Run the batch file to remove all the normal QGIS interface.
-REGEDIT /s StripInterface.reg
 C:\OSGeo4w\bin\qgis-dev.bat --configpath "./app" --optionspath "./app"
