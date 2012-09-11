d2ac37c442b45c7f9b10363de502abbc4a2c0f35
Nathan Woodrow <woodrow.nathan@gmail.com>
2012-09-11 14:27:59 +1000

    Combine install scripts

--- client-setup/install sync framework.bat
+++
@@ -1,3 +0,0 @@
-msiexec /i /passive "Sync Framework 2.1\DatabaseProviders-v3.1-x86-ENU.msi"
-msiexec /i /passive "Sync Framework 2.1\ProviderServices-v2.1-x86-ENU.msi"
-msiexec /i /passive "Sync Framework 2.1\Synchronization-v2.1-x86-ENU.msi"
--- client-setup/install-sql.bat
+++ client-setup/install-sql.bat
@@ -1,4 +1,10 @@
-REM setup.exe /Q /CONFIGURATIONFILE="ConfigurationFile.ini" /IACCEPTSQLSERVERLICENSETERMS
-REM netsh advfirewall firewall add rule name="SQLServer" dir=in action=allow protocol=TCP localport=1433 profile=DOMAIN
+cd /d %~dp0
+
+setup.exe /Q /CONFIGURATIONFILE="ConfigurationFile.ini" /IACCEPTSQLSERVERLICENSETERMS
+netsh advfirewall firewall add rule name="SQLServer" dir=in action=allow protocol=TCP localport=1433 profile=DOMAIN
 REGEDIT /s FieldDataDSN.reg
 SQLCMD -S localhost  -Q "CREATE DATABASE FieldData" 
+
+msiexec /i "Sync Framework 2.1\DatabaseProviders-v3.1-x86-ENU.msi" /passive /norestart
+msiexec /i "Sync Framework 2.1\ProviderServices-v2.1-x86-ENU.msi"/passive /norestart  
+msiexec /i "Sync Framework 2.1\Synchronization-v2.1-x86-ENU.msi"/passive /norestart
