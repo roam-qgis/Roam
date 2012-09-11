5e805366f60e2cf3ecb957fa7711893ab9836661
Nathan Woodrow <woodrow.nathan@gmail.com>
2012-09-11 14:10:59 +1000

    Add database creation

--- client-setup/install-sql.bat
+++ client-setup/install-sql.bat
@@ -1,2 +1,4 @@
-setup.exe /Q /CONFIGURATIONFILE="ConfigurationFile.ini" /IACCEPTSQLSERVERLICENSETERMS
-netsh advfirewall firewall add rule name="SQLServer" dir=in action=allow protocol=TCP localport=1433 profile=DOMAIN
+REM setup.exe /Q /CONFIGURATIONFILE="ConfigurationFile.ini" /IACCEPTSQLSERVERLICENSETERMS
+REM netsh advfirewall firewall add rule name="SQLServer" dir=in action=allow protocol=TCP localport=1433 profile=DOMAIN
+REGEDIT /s FieldDataDSN.reg
+SQLCMD -S localhost  -Q "CREATE DATABASE FieldData" 
