2525c2fbf2ab7de1c00b4d732557bbedac3e2112
Nathan Woodrow <woodrow.nathan@gmail.com>
2012-10-03 13:54:39 +1000

    Setup variables for building:

--- make_win.bat
+++ make_win.bat
@@ -1,7 +1,15 @@
 @ECHO OFF
+
+set OSGEO4W_ROOT=C:\OSGeo4W
+PATH=%OSGEO4W_ROOT%\bin;%PATH%
+for %%f in (%OSGEO4W_ROOT%\etc\ini\*.bat) do call %%f
+
 set PYTHONPATH=C:\OSGeo4W\apps\qgis-dev\python
 Set PATH=C:\OSGeo4W\apps\qgis-dev\bin;%PATH%
 set QGISHOME=C:\OSGeo4W\apps\qgis-dev\
 
-python build.py --target=Sample --with-tests=False deploy
+python build.py --target=Sample ^
+		--with-mssyncing=False ^
+		--with-docs=False ^
+		--with-tests=False deploy
 pause
