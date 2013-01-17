636e75333286031523612ca5c84cfd1abe1de450
Nathan Woodrow <woodrow.nathan@gmail.com>
2013-01-17 16:50:37 +1000

    Reprovison more then one table in upgrade

--- client-manager/client-setup/upgradesync.bat
+++ client-manager/client-setup/upgradesync.bat
@@ -1,10 +1,16 @@
 @ECHO OFF
 
+set TABLE_LIST=(WaterJobs, SewerJobs, ParkAssets)
 set SERVER="Data Source=localhost;Initial Catalog=FieldData;Integrated Security=SSPI;"
 set CLIENT="Data Source=localhost;Initial Catalog=SpatialData;Integrated Security=SSPI;"
 
 sqlcmd -S SD0302 -d SpatialData -i backuptables.sql
-provisioner.exe --server=%SERVER% --client=%CLIENT% --table="WaterJobs" --direction=UploadAndDownload --reprovision
+
+for %%i in %TABLE_LIST% DO (
+	ECHO Provisioning %%i
+	provisioner.exe --server=%SERVER% --client=%CLIENT% --table=%%i --reprovision --direction=UploadAndDownload
+	)
+
 syncer.exe --server=%SERVER% --client=%CLIENT% --reprovision
 sqlcmd -S SD0302 -d SpatialData -i waterjobs.sql
 syncer.exe --server=%SERVER% --client=%CLIENT% --reprovision
