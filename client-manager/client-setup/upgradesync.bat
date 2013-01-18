f55b2b798ca3f2bfb05733ac41a5729a1489e060
Nathan Woodrow <woodrow.nathan@gmail.com>
2013-01-18 14:56:52 +1000

    Do full update in upgradesync.bat

--- client-manager/client-setup/upgradesync.bat
+++ client-manager/client-setup/upgradesync.bat
@@ -1,21 +1,27 @@
 @ECHO OFF
 
-set clientarg=%1
-
 REM Update the client to use the new table per scope syncing style.
 REM This allows us to update clients one by one by not loose data between the sync.
 REM Only handles new records not changed data on client.
 
-
-set TABLE_LIST=(WaterJobs, ParkAssets)
+set clientarg=%1
 set SERVER="Data Source=localhost;Initial Catalog=FieldData;Integrated Security=SSPI;"
 set CLIENT="Data Source=%clientarg%;Initial Catalog=SpatialData;Integrated Security=SSPI;"
+set TABLE_LIST=(AddressNumbers WaterMains WaterServices Cadastre RoadLabels LocalityBoundaries SewerNodes SewerPipes Towns WaterFittings WaterMeters)
+set TWOWAYTABLE_LIST=(WaterJobs SewerJobs ParkAssets)
+
+REM Backup the twoway tables and update them first.
 
 sqlcmd -S SD0302 -d SpatialData -i backuptables.sql
 
-for %%i in %TABLE_LIST% DO (
+for %%i in %TWOWAYTABLE_LIST% DO (
 	ECHO Provisioning %%i
 	provisioner.exe --server=%SERVER% --client=%CLIENT% --table=%%i --reprovision --direction=UploadAndDownload
+	)
+
+for %%i in %TABLE_LIST% DO (
+	ECHO Provisioning %%i
+	provisioner.exe --server=%SERVER% --client=%CLIENT% --table=%%i --reprovision
 	)
 
 syncer.exe --server=%SERVER% --client=%CLIENT%
