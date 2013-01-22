9e6895683cf3aa5de16d6e1fefeefb4388244b63
Nathan Woodrow <woodrow.nathan@gmail.com>
2013-01-22 10:32:37 +1000

    Use setenv.bat in provision batch file

--- client-manager/client-setup/provisionclient_example.bat
+++ client-manager/client-setup/provisionclient_example.bat
@@ -1,21 +1,15 @@
 @ECHO OFF 
 
-set SERVER="Data Source=localhost;Initial Catalog=FieldData;Integrated Security=SSPI;"
-set CLIENT="Data Source=localhost;Initial Catalog=SpatialData;Integrated Security=SSPI;"
+set clientarg=%1
 
-set TABLE_LIST=(AddressNumbers WaterMains WaterServices Cadastre RoadLabels LocalityBoundaries SewerNodes SewerPipes Towns WaterFittings WaterMeters)
-set TWOWAYTABLE_LIST=(WaterJobs SewerJobs ParkAssets)
+CALL setenv.bat
 
 for %%i in %TABLE_LIST% DO (
 	ECHO Provisioning %%i
-	REM Uncomment then following line if you need to reprovision the tables
-	REM provisioner.exe --server=%SERVER% --client=%CLIENT% --table=%%i --srid=28356 --deprovision
-	provisioner.exe --server=%SERVER% --client=%CLIENT% --table=%%i --srid=28356
+	provisioner.exe %CONN%  --scope=%%i
 )
 
 for %%i in %TWOWAYTABLE_LIST% DO (
 	ECHO Provisioning %%i
-	REM Uncomment then following line if you need to reprovision the tables
-	REM provisioner.exe --server=%SERVER% --client=%CLIENT% --table=%%i --srid=28356 --deprovision
-	provisioner.exe --server=%SERVER% --client=%CLIENT% --table=%%i --srid=28356 --direction=UploadAndDownload
+	provisioner.exe %CONN%  --scope=%%i --direction=UploadAndDownload
 )
