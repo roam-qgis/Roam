49cab67bb16b715f8fea459723401a899ae9d99c
Nathan Woodrow <nathan.woodrow@mapsolutions.com.au>
2013-06-11 15:07:49 +1000

    SIP Update

--- QMap Map Manager.bat
+++ QMap Map Manager.bat
@@ -1,3 +1,3 @@
-SET PYTHONPATH=src\qmap\qtcontrols;%PYTHONPATH%
+SET PYTHONPATH=src\qmap\qtcontrols;%PYTHONPATH%;%~dp0
 SET PYQTDESIGNERPATH=src\qmap\qtcontrols\plugins
 "C:\OSGeo4W\bin\qgis-dev.bat" --noplugins %*
--- src/qmap/qtcontrols/plugins/imagewidgetplugin.py
+++ src/qmap/qtcontrols/plugins/imagewidgetplugin.py
@@ -11,7 +11,7 @@ def moduleInformation():
     return "imagewidget", ("QMapImageWidget", )
 
 from PyQt4 import QtGui, QtDesigner
-from imagewidget import QMapImageWidget
+from qmap.qtcontrols.imagewidget import QMapImageWidget
 
 class QMapImageWidgetPlugin(QtDesigner.QPyDesignerCustomWidgetPlugin):
     def __init__(self, parent = None):
