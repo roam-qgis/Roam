f64d09d9eac1a4eb38ba253d093bd128a930dfc9
Nathan Woodrow <nathan.woodrow@mapsolutions.com.au>
2013-07-01 16:20:26 +1000

    Update docs. Update project with shapefile layer. Export shape after sync

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'IntraMaps Roam'
-copyright = u'2013, Nathan Woodrow'
+copyright = u'2013, Digital Mapping Solutions'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
