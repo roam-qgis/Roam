268c241529e4705d4e22dbd56af411ab166878d1
Nathan Woodrow <nathan.woodrow@mapsolutions.com.au>
2013-07-23 17:02:35 +1000

    Update the post install script

--- baseexport.bat
+++ baseexport.bat
@@ -3,4 +3,5 @@ SET HOME=%~dp0export\%NAME%\IntraMaps Roam
 copy /Y "%~dp0installer\_updateconnections.bat" "%HOME%\_updateconnections.bat"
 mkdir "%HOME%\docs"
 copy /Y "%~dp0docs\IntraMaps Roam - User Guide.docx" "%HOME%\docs\IntraMaps Roam - User Guide.docx"
+copy /Y "%~dp0installer\setenv.bat" "%HOME%\setenv.bat"
 xcopy /Y "%~dp0installer\_install" "%HOME%\_install\"
--- src/loader_src/IntraMaps Roam.bat
+++ src/loader_src/IntraMaps Roam.bat
@@ -1,5 +1,3 @@
-@ECHO OFF
-set QGIS_PLUGINPATH=%~dp0
-SET PYTHONPATH=qmap\qtcontrols;%~dp0;%PYTHONPATH%
-SET PYQTDESIGNERPATH=qmap\qtcontrols\plugins
-"C:\OSGeo4W\bin\qgis-dev.bat" --configpath "qmap\app" --optionspath "qmap\app"
+CALL setenv.bat
+
+"%OSGEO4W_ROOT%\bin\qgis-dev.bat" --configpath "qmap\app" --optionspath "qmap\app"
