7aa11987eed66321f6fcedb6fe1d93c00374db2b
Nathan Woodrow <nathan.woodrow@mapsolutions.com.au>
2013-08-15 14:03:39 +1000

    Move export shp ogr2ogr dump to new file'

--- export_melton.bat
+++ export_melton.bat
@@ -11,6 +11,7 @@ call baseexport.bat
 SET PROJECT=%HOME%\projects\melton_firebreak
 move /Y "%PROJECT%\fireinspect.qgs.tmpl" "%PROJECT%\fireinspect.qgs"
 move /Y "%PROJECT%\Sync-All.bat.tmpl" "%PROJECT%\Sync-All.bat"
+move /Y "%PROJECT%\export_shp.bat.tmpl" "%PROJECT%\export_shp.bat"
 move /Y "%PROJECT%\Sync-Inspections.bat.tmpl" "%PROJECT%\Sync-Inspections.bat"
 move /Y "%PROJECT%\_docs\*" "%HOME%\docs\"
 
