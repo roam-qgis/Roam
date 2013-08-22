8c4d600766ad24bc8d2d205520c3f16415958d06
Nathan Woodrow <nathan.woodrow@mapsolutions.com.au>
2013-08-22 16:53:19 +1000

    Create *.sql.tmpl scripts for melton. Remove post install logic

--- export_melton.bat
+++ export_melton.bat
@@ -9,10 +9,6 @@ call baseexport.bat
 : Over write the project file with the template one
 
 SET PROJECT=%HOME%\projects\melton_firebreak
-move /Y "%PROJECT%\fireinspect.qgs.tmpl" "%PROJECT%\fireinspect.qgs"
-move /Y "%PROJECT%\Sync-All.bat.tmpl" "%PROJECT%\Sync-All.bat"
-move /Y "%PROJECT%\export_shp.bat.tmpl" "%PROJECT%\export_shp.bat"
-move /Y "%PROJECT%\Sync-Inspections.bat.tmpl" "%PROJECT%\Sync-Inspections.bat"
 move /Y "%PROJECT%\_docs\*" "%HOME%\docs\"
 
 
