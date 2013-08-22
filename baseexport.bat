0fc089c271c3e5a4baffd35ab540a9febcfcdb78
Nathan Woodrow <nathan.woodrow@mapsolutions.com.au>
2013-08-22 12:48:43 +1000

    Septic tank form updates

--- baseexport.bat
+++ baseexport.bat
@@ -1,6 +1,5 @@
 call %~dp0qmap-admin\build.bat "%NAME%"
 SET HOME=%~dp0export\%NAME%\IntraMaps Roam
-copy /Y "%~dp0installer\_updateconnections.bat" "%HOME%\_updateconnections.bat"
 mkdir "%HOME%\docs"
 copy /Y "%~dp0docs\IntraMaps Roam - User Guide.docx" "%HOME%\docs\IntraMaps Roam - User Guide.docx"
 copy /Y "%~dp0installer\setenv.bat" "%HOME%\setenv.bat"
