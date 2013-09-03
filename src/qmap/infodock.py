69aa1a15b28e8fdace11eb70ede49f8a4acfe948
Nathan Woodrow <nathan.woodrow@mapsolutions.com.au>
2013-09-03 11:44:31 +1000

    Use display field for info results

--- src/qmap/infodock.py
+++ src/qmap/infodock.py
@@ -1,42 +1,89 @@
 import os
+import collections
 
 from PyQt4.QtGui import QTableWidgetItem
 from PyQt4.QtCore import Qt, QUrl
+
+from qgis.core import QgsExpression, QgsFeature
+
+import utils
 
 from uifiles import (infodock_widget, infodock_base)
 
 htmlpath = os.path.join(os.path.dirname(__file__) , "info.html")
 
+with open(htmlpath) as f:
+    template = f.read()
+
 class InfoDock(infodock_widget, infodock_base):
     def __init__(self, parent):
         super(InfoDock, self).__init__(parent)
         self.setupUi(self)
+        self.results = collections.defaultdict(list)
+        self.layerList.currentIndexChanged.connect(self.layerIndexChanged)
+        self.featureList.currentIndexChanged.connect(self.featureIndexChanged)
         
-    def addResult(self, result):
+    def featureIndexChanged(self, index):
+        feature = self.featureList.itemData(index)
+        layer = self.layerList.itemData(self.layerList.currentIndex())
+        if not feature:
+            return 
+        
+        self.update(layer, feature)
+        
+    def layerIndexChanged(self, index):
+        self.featureList.clear()
+        layer = self.layerList.itemData(index)
+        if not layer:
+            return
+        
+        features = self.results[layer]
+        utils.log(features)
+        index = layer.fieldNameIndex(layer.displayField())
+        for feature in features:
+            if index < 0:
+                display = QgsExpression.replaceExpressionText( layer.displayField(), feature, layer )
+            else:
+                display = feature[index]
+            self._addFeature(str(display), feature)
+                     
+    def _addFeature(self, display, feature):
+        self.featureList.addItem(display, feature)
+        
+    def setResults(self, results):
+        self.clearResults()
+        for result in results:
+            self._addResult(result)
+        self.layerIndexChanged(0)
+        self.featureIndexChanged(0)
+            
+    def _addResult(self, result):
         layer = result.mLayer
         self.addLayer(layer)
+        utils.log(result.mFeature.id())
+        self.results[layer].append(QgsFeature(result.mFeature))
         
-        feature = result.mFeature
-        
+    def update(self, layer, feature):
         fields = [field.name() for field in feature.fields()]
         data = dict(zip(fields, feature.attributes()))
-        with open(htmlpath) as f:
-            html = f.read()
         items = []
         for key, value in data.iteritems():
             item = "<tr><th>{}</th> <td>{}</td></tr>".format(key, value)
             items.append(item)
             
         rows = ''.join(items)
-        html = html.format(TITLE=layer.name(), ROWS=rows)
+        html = template.format(TITLE=layer.name(), ROWS=rows)
         base = os.path.dirname(os.path.abspath(__file__))
         baseurl = QUrl.fromLocalFile(base + '\\')
         self.attributesView.setHtml(html, baseurl)
         
     def clearResults(self):
+        self.results.clear()
         self.layerList.clear()
+        self.featureList.clear()
         self.attributesView.setHtml('')
           
     def addLayer(self, layer):
-        self.layerList.addItem(layer.name())
-        
+        name = layer.name()
+        if self.layerList.findData(layer) == -1:
+            self.layerList.addItem(name, layer)
