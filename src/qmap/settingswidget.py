5bdb48296b1d2c60a2b30b84fccc2a929738aa6e
Nathan Woodrow <nathan.woodrow@mapsolutions.com.au>
2013-09-04 11:06:12 +1000

    Read settings into settings dialog, read version from version.txt

--- src/qmap/settingswidget.py
+++ src/qmap/settingswidget.py
@@ -1,6 +1,37 @@
+import os
+
+from qgis.core import QgsGPSDetector
+
 from uifiles import settings_widget, settings_base
+from utils import settings, curdir
 
 class SettingsWidget(settings_widget, settings_base):
     def __init__(self, parent=None):
         super(SettingsWidget, self).__init__(parent)
         self.setupUi(self)
+        
+    def readSettings(self):
+        fullscreen = settings.get("fullscreen", False)
+        gpsport = settings.get("gpsport", 'scan')
+        self.fullScreenCheck.setChecked(fullscreen)
+        gpsindex = self.gpsPortCombo.findText(gpsport)
+        if gpsindex == -1:
+            self.gpsPortCombo.addItem(gpsport)
+        else:
+            self.gpsPortCombo.setCurrentIndex(gpsindex)
+        
+    def populateControls(self):
+        # First item is the local gpsd so skip that. 
+        ports = QgsGPSDetector.availablePorts()[1:]
+        self.gpsPortCombo.addItem('scan', 'scan')
+        for port, name in ports:
+            self.gpsPortCombo.addItem(name, port)
+            
+        # Read version
+        try:
+            with open(os.path.join(curdir, 'version.txt')) as f:
+                version = f.read().strip()
+        except IOError:
+            version = 'unknown'
+            
+        self.versionLabel.setText(version)
