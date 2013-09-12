7d395155fd6efab34ba4c1a990dfadc99bc186f9
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2013-09-12 12:38:11 +1000

    Allow for dragging a box when using inspection tool

---
+++ src/qmap/maptools/maptoolutils.py
@@ -0,0 +1,14 @@
+from qgis.core import QGis, QgsPoint
+    
+def setRubberBand(canvas, selectRect, rubberBand):
+    transform = canvas.getCoordinateTransform()
+    lowerleft = transform.toMapCoordinates( selectRect.left(), selectRect.bottom() )
+    upperright = transform.toMapCoordinates( selectRect.right(), selectRect.top() )
+    
+    if rubberBand:
+        rubberBand.reset( QGis.Polygon );
+        rubberBand.addPoint( lowerleft, False);
+        rubberBand.addPoint( QgsPoint( upperright.x(), lowerleft.y() ), False);
+        rubberBand.addPoint( upperright, False);
+        rubberBand.addPoint( QgsPoint( lowerleft.x(), upperright.y() ), True );
+    
