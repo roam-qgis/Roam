dc962c14d1ea2e7bc4515b8619f00bb579ccee48
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2013-09-13 15:27:35 +1000

    Add drag area support to edit tool

--- src/qmap/maptools/edittool.py
+++ src/qmap/maptools/edittool.py
@@ -1,13 +1,13 @@
-
-from PyQt4.QtCore import pyqtSignal
+from PyQt4.QtCore import pyqtSignal, QRect, Qt
 from PyQt4.QtGui import QCursor, QPixmap, QColor
 from qgis.core import (QgsRectangle, QgsTolerance, 
                        QgsFeatureRequest, QgsFeature,
-                       QgsVectorLayer)
+                       QgsVectorLayer, QGis)
 from qgis.gui import QgsMapTool, QgsRubberBand
 
 from qmap.listfeatureform import ListFeaturesForm
 from maptool import MapTool
+import maptoolutils
 
 class EditTool(MapTool):
     """
@@ -25,6 +25,11 @@ class EditTool(MapTool):
         self.band = QgsRubberBand(self.canvas)
         self.band.setColor(QColor.fromRgb(224,162,16))
         self.band.setWidth(3)
+        
+        self.selectband = None
+        
+        self.selectrect = QRect()
+        self.dragging = False
 
         self.cursor = QCursor(QPixmap(["16 16 3 1",
             "      c None",
@@ -47,7 +52,17 @@ class EditTool(MapTool):
             "      ++.++     ",
             "       +.+      "]))
         
-    def getFeatures(self, point):
+    def getFeatures(self, rect):
+        rq = QgsFeatureRequest().setFilterRect(rect)
+
+        self.band.reset()
+        for layer in self.layers:
+            rq = QgsFeatureRequest().setFilterRect(rect) 
+            for feature in layer.getFeatures(rq):
+                if feature.isValid():
+                    yield feature, layer
+                    
+    def toSearchRect(self, point):
         searchRadius =  self.canvas.extent().width() * ( self.radius / 100.0 )
 
         point = self.toMapCoordinates(point)
@@ -57,22 +72,37 @@ class EditTool(MapTool):
         rect.setXMaximum(point.x() + searchRadius)
         rect.setYMinimum(point.y() - searchRadius)
         rect.setYMaximum(point.y() + searchRadius)
-        
-        rq = QgsFeatureRequest().setFilterRect(rect)
+        return rect
 
-        self.band.reset()
-        for layer in self.layers:
-            rq = QgsFeatureRequest().setFilterRect(rect) 
-            for feature in layer.getFeatures(rq):
-                if feature.isValid():
-                    yield feature, layer
+    def canvasPressEvent(self, event):
+        self.selectrect.setRect( 0, 0, 0, 0 )
+        
+        self.selectband = QgsRubberBand(self.canvas, QGis.Polygon )
+        self.selectband.setColor(QColor.fromRgb(0,0,255, 65))
+        self.selectband.setWidth(5)
         
     def canvasMoveEvent(self, event):
-        for feature, _ in self.getFeatures(point = event.pos()):
-            self.band.addGeometry(feature.geometry(), None)
+        if not event.buttons() == Qt.LeftButton:
+            return
+        
+        if not self.dragging:
+            self.dragging = True
+            self.selectrect.setTopLeft(event.pos())
+        self.selectrect.setBottomRight(event.pos())
+        
+        maptoolutils.setRubberBand(self.canvas, self.selectrect, self.selectband)  
                      
     def canvasReleaseEvent(self, event):
-        features = list(self.getFeatures(point = event.pos()))
+        if self.dragging:
+            geometry = self.selectband.asGeometry()
+            rect = geometry.boundingBox()            
+        else:
+            rect = self.toSearchRect(event.pos())
+            
+        self.dragging = False
+        self.selectband.reset()
+            
+        features = list(self.getFeatures(rect))
             
         if len(features) == 1:
             feature = features[0]
