6fe39564c9a2fbbde8439991978c6e424a139a5a
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2013-09-18 13:11:45 +1000

    Add QGIS paths for qgis.core, qgis.gui

--- src/setenv.bat
+++ src/setenv.bat
@@ -7,9 +7,9 @@ SET OSGEO4W_ROOT=C:\OSGeo4W
 
 :: SET OSGEO4W_ROOT=%~dp0qgis
 
-PATH=%OSGEO4W_ROOT%\bin;%PATH%
+PATH=%OSGEO4W_ROOT%\bin;%OSGEO4W_ROOT%\apps\qgis-dev\bin;%PATH%
 for %%f in ("%OSGEO4W_ROOT%\etc\ini\*.bat") do call "%%f"
 
 SET QGIS_PLUGINPATH=%~dp0
-SET PYTHONPATH=%~dp0qmap\qtcontrols;%~dp0;%PYTHONPATH%
+SET PYTHONPATH=%~dp0qmap\qtcontrols;%~dp0;%OSGEO4W_ROOT%\apps\qgis-dev\python;%PYTHONPATH%
 SET PYQTDESIGNERPATH=%~dp0qmap\qtcontrols\plugins
