7ecb7a22159dc61e9d7288b25abc1f6e9b1a2efd
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2013-09-18 13:03:58 +1000

    Fix path in setenv.bat

--- src/setenv.bat
+++ src/setenv.bat
@@ -7,8 +7,7 @@ SET OSGEO4W_ROOT=C:\OSGeo4W
 
 :: SET OSGEO4W_ROOT=%~dp0qgis
 
-
-PATH="%OSGEO4W_ROOT%\bin";%PATH%
+PATH=%OSGEO4W_ROOT%\bin;%PATH%
 for %%f in ("%OSGEO4W_ROOT%\etc\ini\*.bat") do call "%%f"
 
 SET QGIS_PLUGINPATH=%~dp0
