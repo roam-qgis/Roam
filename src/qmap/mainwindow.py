8da9794115eac3a5a4ae9a86da7cc3d2a39e098d
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2013-11-01 17:07:50 +1000

    Add spacer widgets for toolbars

--- src/qmap/mainwindow.py
+++ src/qmap/mainwindow.py
@@ -1,5 +1,5 @@
 from PyQt4.QtCore import Qt
-from PyQt4.QtGui import QActionGroup
+from PyQt4.QtGui import QActionGroup, QWidget, QSizePolicy, QLabel
 from qgis.core import QgsPalLabeling
 
 import sys
@@ -31,6 +31,41 @@ class MainWindow(mainwindow_widget, mainwindow_base):
 
         self.projectwidget = ProjectsWidget(self)
         self.project_page.layout().addWidget(self.projectwidget)
+
+        def createSpacer(width):
+            widget = QWidget()
+            widget.setMinimumWidth(width)
+            return widget
+
+        spacewidget = createSpacer(60)
+        gpsspacewidget = createSpacer(30)
+        sidespacewidget = createSpacer(30)
+        sidespacewidget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        gpsspacewidget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.projecttoolbar.insertWidget(self.actionSync, gpsspacewidget)
+        self.projecttoolbar.insertWidget(self.actionHome, spacewidget)
+
+        self.projectlabel, self.userlabel = self._createSideLabels()
+
+        labelaction = self.menutoolbar.insertWidget(self.actionSettings, self.userlabel)
+
+        self.menutoolbar.insertWidget(labelaction, sidespacewidget)
+        self.menutoolbar.insertWidget(labelaction, self.projectlabel)
+
+    def _createSideLabels(self):
+        style = """
+            QLabel {
+                    color: #8c8c8c;
+                    font: 10px "Calibri" ;
+                    }"""
+        projectlabel = QLabel("Project: <br> None")
+        projectlabel.setAlignment(Qt.AlignCenter)
+        projectlabel.setStyleSheet(style)
+
+        userlabel = QLabel("User: <br> {user}")
+        userlabel.setAlignment(Qt.AlignCenter)
+        userlabel.setStyleSheet(style)
+        return projectlabel, userlabel
 
     def loadProjectList(self, projects):
         self.projectwidget.loadProjectList(projects)
