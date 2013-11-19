35a4253ab043da10c5ec4b5a16b324e22951a66f
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2013-11-19 14:57:31 +1000

    Add setenv batch file.

--- src/IntraMaps Roam - Standalone.bat
+++ src/IntraMaps Roam - Standalone.bat
@@ -1,34 +1,9 @@
 @ECHO OFF
 setlocal enableextensions enabledelayedexpansion
 
-SET OSGEO4W_ROOT=C:\OSGeo4W
-SET QGIS=%OSGEO4W_ROOT%\apps\qgis
+CALL "%~dp0setenv.bat"
 
-: Look for QGIS in Program files if it's not in the normal place
-IF NOT EXIST %QGIS% (
-    SET OSGEO4W_ROOT=C:\Program Files\QGIS Dufour
-    SET QGIS=!OSGEO4W_ROOT!\apps\qgis
-)
-
-set QGIS_PREFIX_PATH=!QGIS!
-
-: GDAL Setup
-SET GDAL_DATA=!OSGEO4W_ROOT!\share\gdal
-SET GDAL_DRIVER_PATH=!OSGEO4W_ROOT!\bin\gdalplugins
-
-: Python Setup
-set PATH=!OSGEO4W_ROOT!\bin;!QGIS!\bin;%~dp0;!PATH!
-SET PYTHONHOME=!OSGEO4W_ROOT!\apps\Python27
-set PYTHONPATH=%~dp0;!QGIS!\python;!~dp0qmap\editorwidgets\uifiles\;!PYTHONPATH!
-
-: QT Setup
-set QT_PLUGIN_PATH=!OSGEO4W_ROOT!\apps\Qt4\plugins
-
-:: set QGIS_DEBUG=0
-:: set QGIS_LOG_FILE=qgis.log
-:: set QGIS_DEBUG_FILE=qgis-debug.log
-
-python qmap "%~dp0\projects"
+python qmap "%~dp0\..\projects"
 
 pause
 
