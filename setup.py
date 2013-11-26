7d887e14e106a71b9eafda889f501db6060a7419
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2013-11-26 10:00:36 +1000

    Add note in package readme about osgeo4w paths Include tests in output Copy imageformats to dist folder Add logging for tests module.

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from distutils.core import setup
 import py2exe
 import glob
+import os
 
 from py2exe.build_exe import py2exe as build_exe
+
+osgeopath = r'C:\OSGeo4W'
+qtimageforms = os.path.join(osgeopath,r'apps\qt4\plugins\imageformats\*')
 
 datafiles = [(".", [r'src\settings.config']),
             (r'libs\qmap', [r'src\qmap\info.html',
                             r'src\qmap\error.html']),
             (r'libs\qmap\bootstrap', glob.glob(r'src\qmap\bootstrap\*')),
-            (r'projects', [r'projects\__init__.py'])]
+            (r'projects', [r'projects\__init__.py']),
+            # We have to copy the imageformat drivers to the root
+            # folder.
+            (r'imageformats', glob.glob(qtimageforms))]
 
 roam_target = dict(
                 script='src\qmap\__main__.py',
@@ -16,19 +23,25 @@ roam_target = dict(
                 icon_resources=[(1, "src\icon.ico")]
             )
 
+tests_target = dict(
+                script='tests\__main__.py',
+                dest_base='Roam_tests',
+                icon_resources=[(1, "src\icon.ico")]
+            )
+
 setup(
     name='qmap',
     version='2.0',
     packages=['qmap', 'qmap.yaml', 'qmap.syncing', 'qmap.maptools', 'qmap.editorwidgets', 'qmap.editorwidgets.core',
-              'qmap.editorwidgets.uifiles', '_install'],
-    package_dir={'': 'src'},
+              'qmap.editorwidgets.uifiles', '_install', 'tests'],
+    package_dir={'': 'src', 'tests' : 'tests'},
     url='',
     license='GPL',
     author='nathan.woodrow',
     author_email='nathan.woodrow@mapsolutions.com.au',
     description='',
     windows=[roam_target],
-    console=['src\_install\postinstall.py'],
+    console=['src\_install\postinstall.py', tests_target],
     data_files=datafiles,
     zipfile='libs\\',
     options = {'py2exe': {
---
+++ tests/__main__.py
@@ -0,0 +1,31 @@
+__author__ = 'nathan.woodrow'
+
+import logging
+import sys
+
+def excepthook(errorhandler, exctype, value, traceback):
+    errorhandler(exctype, value, traceback)
+    logger.debug("Uncaught exception", exc_info=(exctype, value, traceback))
+
+LOG_FILENAME = 'roam-tests.log'
+log_format = '%(levelname)s - %(asctime)s - %(module)s-%(funcName)s:%(lineno)d - %(message)s'
+console_format = '%(levelname)s %(module)s-%(funcName)s:%(lineno)d - %(message)s'
+formater = logging.Formatter(log_format)
+console_formater = logging.Formatter(console_format)
+
+filehandler = logging.FileHandler(LOG_FILENAME, mode='at')
+filehandler.setLevel(logging.DEBUG)
+filehandler.setFormatter(formater)
+
+stream = logging.StreamHandler(stream=sys.stdout)
+stream.setLevel(logging.DEBUG)
+stream.setFormatter(console_formater)
+
+logger = logging.getLogger("roam")
+logger.addHandler(filehandler)
+logger.addHandler(stream)
+logger.setLevel(logging.DEBUG)
+
+logger.info('Running Tests')
+
+sys.excepthook = excepthook
