948eaa205e0d1435c18673a8ee6da65054989511
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2013-11-28 10:48:47 +1000

    Log packaging to file

--- package.bat
+++ package.bat
@@ -1,8 +1,18 @@
 @ECHO OFF
 
-call %~dp0setenv.bat
+CD %~dp0
 
-call build.bat
-python setup.py py2exe
+call setenv.bat
+
+ECHO Building Roam package
+
+>package.log 2>&1 (
+	python build.py clean
+	python build.py
+	python setup.py py2exe
+)
+
+ECHO Package in dist\
+ECHO Check package.log for build log and errors
 
 if defined DOUBLECLICKED pause
