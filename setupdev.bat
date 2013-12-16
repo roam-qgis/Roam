932f6cfa9ba36757df853c641c46704210f73d78
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2013-12-16 20:43:29 +1000

    install easy_install and pip. Use requirements.txt for pip

--- setupdev.bat
+++ setupdev.bat
@@ -4,12 +4,9 @@ IF NOT EXIST "%VS90COMNTOOLS%" GOTO NOVS
 CALL "C:\OSGeo4W\bin\o4w_env.bat"
 
 pushd %~dp0
-mkdir temp
-pushd temp
-curl "http://waix.dl.sourceforge.net/project/py2exe/py2exe/0.6.9/py2exe-0.6.9.zip" > py2exe.zip
-unzip -o py2exe.zip
-pushd py2exe-0.6.9
-python setup.py install
+curl https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -k | python
+C:\OSGeo4W\apps\Python27\Scripts\easy_install.exe pip
+pip install -r requirements.txt
 popd && popd
 GOTO END
 
