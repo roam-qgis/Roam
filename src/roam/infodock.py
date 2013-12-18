597ac4349d195237f6f34d7c00d47151537e9736
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2013-12-18 16:18:19 +1000

    Fix wrap around in info cursor

--- src/roam/infodock.py
+++ src/roam/infodock.py
@@ -33,36 +33,38 @@ class FeatureCursor(object):
     """
     A feature cursor that keeps track of the current feature
     and handles wrapping to the start and end of the list
+
+    HACK: This could be a lot nicer and cleaner but it works
+    for now
     """
-    def __init__(self, layer, features, form=None, position=0):
+    def __init__(self, layer, features, form=None, index=0):
         self.layer = layer
         self.features = features
-        self.position = position
+        self.index = index
         self.form = form
 
     def next(self):
-        if self.position == len(self.features):
-            self.position = 0
-        else:
-            self.position += 1
+        self.index += 1
+        if self.index > len(self.features) - 1:
+            self.index = 0
+        print self.index, len(self.features)
         return self
 
     def back(self):
-        if self.position == 0:
-            self.position = len(self.features)
-        else:
-            self.position -= 1
+        self.index -= 1
+        if self.index < 0:
+            self.index = len(self.features) - 1
         return self
 
     @property
     def feature(self):
         try:
-            return self.features[self.position]
+            return self.features[self.index]
         except IndexError:
             return None
 
     def __str__(self):
-        return "{} of {}".format(self.position, len(self.features))
+        return "{} of {}".format(self.index + 1, len(self.features))
 
 class InfoDock(infodock_widget, QWidget):
     requestopenform = pyqtSignal(object, QgsFeature)
@@ -131,7 +133,6 @@ class InfoDock(infodock_widget, QWidget):
             return
 
         cursor = item.data(Qt.UserRole)
-        self.countlabel.setText(str(cursor))
         self.update(cursor)
 
     def setResults(self, results, forms):
@@ -143,25 +144,26 @@ class InfoDock(infodock_widget, QWidget):
 
         self.layerList.setCurrentRow(0)
         self.layerList.setMinimumWidth(self.layerList.sizeHintForColumn(0) + 20)
+        self.navwidget.show()
 
     def _addResult(self, layer, features):
-        name = layer.name()
-        forms = self.forms.get(name, [])
+        layername = layer.name()
+        forms = self.forms.get(layername, [])
         if not forms:
-            item = QListWidgetItem(QIcon(), name, self.layerList)
+            item = QListWidgetItem(QIcon(), layername, self.layerList)
             item.setData(Qt.UserRole, FeatureCursor(layer, features))
             return
 
         for form in forms:
-            print form.label
-            name = "{} ({})".format(name, form.label)
+            itemtext = "{} ({})".format(layername, form.label)
             icon = QIcon(form.icon)
-            item = QListWidgetItem(icon, name, self.layerList)
+            item = QListWidgetItem(icon, itemtext, self.layerList)
             item.setData(Qt.UserRole, FeatureCursor(layer, features, form))
 
     def update(self, cursor):
         global image
         images = {}
+        feature = cursor.feature
         fields = [field.name() for field in feature.fields()]
         data = OrderedDict()
 
@@ -176,7 +178,7 @@ class InfoDock(infodock_widget, QWidget):
         form = cursor.form
         layer = cursor.layer
         if form:
-            name = "{}({})".format(layer.name(), form.label)
+            name = "{}".format(layer.name(), form.label)
         else:
             name = layer.name()
 
@@ -187,8 +189,14 @@ class InfoDock(infodock_widget, QWidget):
         base = os.path.dirname(os.path.abspath(__file__))
         baseurl = QUrl.fromLocalFile(base + '\\')
 
-        displaytext = form.settings.get('display', None)
-        display = QgsExpression.replaceExpressionText(displaytext, cursor.feature, layer )
+        if form:
+            displaytext = form.settings.get('display', None)
+            display = QgsExpression.replaceExpressionText(displaytext, cursor.feature, layer )
+        else:
+            display = str(feature[0])
+
+        self.countlabel.setText(str(cursor))
+        self.displaylabel.setText(display)
         self.attributesView.setHtml(html, baseurl)
         self.editButton.setVisible(not form is None)
         self.featureupdated.emit(layer, cursor.feature, cursor.features)
@@ -198,4 +206,5 @@ class InfoDock(infodock_widget, QWidget):
         self.attributesView.setHtml('')
         self.editButton.setVisible(False)
         self.resultscleared.emit()
+        self.navwidget.hide()
 
