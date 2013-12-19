c3db8df1df3add5c71af984dfa3127cb59f2fbd3
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2013-12-19 17:09:33 +1000

    Add some checkbox widget tests.

---
+++ tests/tests_editorwidgets/test_checkboxwidget.py
@@ -0,0 +1,43 @@
+import pytest
+
+from PyQt4.QtGui import QCheckBox, QApplication
+
+from roam.editorwidgets.checkboxwidget import CheckboxWidget
+
+app = None
+
+config = {"checkedvalue":'MyTrue',
+          "uncheckedvalue": 'MyFalse'}
+
+def setup_module(module):
+    global app
+    app = QApplication([])
+    print app
+
+def test_should_set_config_checkedvalue_on_true():
+    checkbox = QCheckBox()
+    assert not checkbox.isChecked()
+    widget = CheckboxWidget(widget=checkbox)
+    widget.config = config
+    widget.setvalue('MyTrue')
+    assert checkbox.isChecked()
+
+def test_should_set_config_uncheckedvalue_on_false():
+    checkbox = QCheckBox()
+    checkbox.setChecked(True)
+    assert checkbox.isChecked()
+    widget = CheckboxWidget(widget=checkbox)
+    widget.config = config
+    widget.setvalue('MyFalse')
+    assert not checkbox.isChecked()
+
+def test_should_return_uncheckedvalue_on_false():
+    checkbox = QCheckBox()
+    widget = CheckboxWidget(widget=checkbox)
+    widget.config = config
+    widget.setvalue('MyFalse')
+    assert checkbox.isChecked() == False
+    assert widget.value() == 'MyFalse'
+
+def teardown_module(module):
+    app.exit()
