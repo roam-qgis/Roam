6c079de75633e2d4001d3c869ebf44c3d17ab03d
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2014-01-09 14:07:34 +1000

    Fix build script

--- setupdev.bat
+++ setupdev.bat
@@ -7,7 +7,11 @@ pushd %~dp0
 curl https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -k | python
 C:\OSGeo4W\apps\Python27\Scripts\easy_install.exe pip
 pip install -r requirements.txt
-popd && popd
+curl "http://waix.dl.sourceforge.net/project/py2exe/py2exe/0.6.9/py2exe-0.6.9.zip" > py2exe.zip
+unzip -o py2exe.zip
+pushd py2exe-0.6.9
+python setup.py install
+popd
 GOTO END
 
 :NOVS
