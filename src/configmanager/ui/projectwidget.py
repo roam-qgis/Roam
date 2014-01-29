a78e4d18937c91d2ed7332161aa12b43c7a2d7d1
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2014-01-29 11:59:47 +1000

    Load forms into form list

--- src/configmanager/ui/projectwidget.py
+++ src/configmanager/ui/projectwidget.py
@@ -1,4 +1,6 @@
-from PyQt4.QtGui import QWidget
+from PyQt4.QtCore import Qt
+from PyQt4.QtGui import QWidget, QStandardItemModel, QStandardItem, QIcon
+
 from configmanager.ui.ui_projectwidget import Ui_Form
 
 
@@ -6,6 +8,8 @@ class ProjectWidget(Ui_Form, QWidget):
     def __init__(self, parent=None):
         super(ProjectWidget, self).__init__(parent)
         self.setupUi(self)
+        self.formmodel = QStandardItemModel()
+        self.formlist.setModel(self.formmodel)
 
     def setproject(self, project):
         self.project = project
@@ -13,5 +17,17 @@ class ProjectWidget(Ui_Form, QWidget):
 
     def _updateforproject(self, project):
         self.titleText.setText(project.name)
+        self.descriptionText.setPlainText(project.description)
+        self._loadforms(project.forms)
+
+    def _loadforms(self, forms):
+        self.formmodel.clear()
+        for form in forms:
+            item = QStandardItem(form.name)
+            item.setIcon(QIcon(form.icon))
+            item.setData(form, Qt.UserRole)
+            self.formmodel.appendRow(item)
+
+
 
 
