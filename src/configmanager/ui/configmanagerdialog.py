0e99e92892d2e46d7cb01da47df0dbd8b0bd6b94
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2014-03-05 12:13:53 +1000

    Add dialog for new form name. Fix #59

--- src/configmanager/ui/configmanagerdialog.py
+++ src/configmanager/ui/configmanagerdialog.py
@@ -5,7 +5,7 @@ import traceback
 
 from datetime import datetime
 
-from PyQt4.QtGui import QDialog, QFont, QColor, QIcon, QMessageBox, QStandardItem, QStandardItemModel
+from PyQt4.QtGui import QDialog, QFont, QColor, QIcon, QMessageBox, QStandardItem, QStandardItemModel, QInputDialog
 from PyQt4.QtCore import QAbstractItemModel, QModelIndex, Qt
 
 
@@ -19,28 +19,41 @@ import roam.messagebaritems
 
 templatefolder = os.path.join(os.path.dirname(__file__), "..", "templates")
 
+def newfoldername(basetext, basefolder, formtype, alreadyexists=False):
+    message = "Please enter a new folder name for the {}".format(formtype)
+    if alreadyexists:
+        message += "<br> {} folder already exists please select a new one.".format(formtype)
+
+    name, ok = QInputDialog.getText(None, "New {} folder name".format(formtype), message)
+
+    if not ok or not name:
+        return "{}_{}".format(basetext, datetime.today().strftime('%d%m%y%f'))
+    else:
+        name = name.replace(" ", "_")
+
+    if os.path.exists(os.path.join(basefolder, name)):
+        return newfoldername(basetext, basefolder, formtype, alreadyexists=True)
+
+    return name
+
 def newproject(projectfolder):
     """
     Create a new folder in the projects folder.
     :param projectfolder: The root project folder
     :return: The new project that was created
     """
-    def newname():
-        return "newProject{}".format(datetime.today().strftime('%d%m%y%f'))
-
+    foldername = newfoldername("project", projectfolder, "Project")
     templateproject = os.path.join(templatefolder, "templateProject")
-    newfolder = os.path.join(projectfolder, newname())
+    newfolder = os.path.join(projectfolder, foldername)
     shutil.copytree(templateproject, newfolder)
     project = roam.project.Project.from_folder(newfolder)
     project.settings['forms'] = {}
     return project
 
 def newform(project):
-    def newformname():
-        return "form_{}".format(datetime.today().strftime('%d%m%y%f'))
 
     folder = project.folder
-    foldername = newformname()
+    foldername = newfoldername("form", folder, "Form")
 
     formfolder = os.path.join(folder, foldername)
     templateform = os.path.join(templatefolder, "templateform")
