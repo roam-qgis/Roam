ef04121c7474717b3b8fdfadce547ccdcb5c2456
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2014-04-03 10:30:45 +1000

    Comments

--- src/roam/events.py
+++ src/roam/events.py
@@ -1,3 +1,8 @@
+"""
+RoamEvents is an event sink for common signals used though out Roam.
+
+These can be raised and handled anywhere in the application.
+"""
 from PyQt4.QtCore import pyqtSignal, QObject, QUrl
 
 from qgis.core import QgsFeature, QgsPoint
@@ -5,8 +10,14 @@ from qgis.core import QgsFeature, QgsPoint
 class _Events(QObject):
     # Emit when you need to open a image in the main window
     openimage = pyqtSignal(object)
+
+    #Emit to open a url
     openurl = pyqtSignal(QUrl)
+
+    # Emit when requesting to open a feature form
     openfeatureform = pyqtSignal(object, QgsFeature)
+
+    # Connect to listen to GPS status updates.
     gpspostion = pyqtSignal(QgsPoint, object)
 
 RoamEvents = _Events()
