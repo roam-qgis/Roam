a77560f4708565ec98723fa564e8f3e263f378ec
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2014-04-22 09:18:30 +1000

    Follow up f2f3c79. Rework config manager __main__

--- src/configmanager/__main__.py
+++ src/configmanager/__main__.py
@@ -1,45 +1,31 @@
 import os
 import sys
-
-from functools import partial
-
-from qgis.core import QgsApplication, QgsProviderRegistry, QGis
-
-from PyQt4.QtGui import QApplication, QFont, QIcon
+import functools
 
 srcpath = os.path.dirname(os.path.realpath(sys.argv[0]))
 sys.path.append(srcpath)
 
-import roam
 import roam.environ
+import roam.config
+
+roamapp = roam.environ.setup(sys.argv)
+roam.config.load(roamapp.settingspath)
+
+import roam
 import roam.project
 import roam.resources_rc
-
-from configmanager.ui.configmanagerdialog import ConfigManagerDialog
-
 import configmanager.logger as logger
 
+from PyQt4.QtGui import QApplication, QFont, QIcon
+from qgis.core import QGis
+from configmanager.ui.configmanagerdialog import ConfigManagerDialog
+
 logger.info("Loading Roam Config Manager")
-roamapp = roam.environ.setup(sys.argv)
-
-app = QgsApplication(sys.argv, True)
-QgsApplication.setPrefixPath(roamapp.prefixpath, True)
-QgsApplication.initQgis()
-
-logger.info(QgsApplication.showSettings())
-logger.info(QgsProviderRegistry.instance().pluginList())
-logger.info(QgsApplication.libraryPaths())
-
 logger.info("Roam Version: {}".format(roam.__version__))
 logger.info("QGIS Version: {}".format(str(QGis.QGIS_VERSION)))
 
-QApplication.setStyle("Plastique")
-QApplication.setFont(QFont('Segoe UI'))
 QApplication.setWindowIcon(QIcon(':/branding/logo'))
 QApplication.setApplicationName("IntraMaps Roam Config Manager")
-
-import roam.config
-roam.config.load(roamapp.settingspath)
 
 projectpaths = roam.environ.projectpaths(sys.argv, roamapp)
 projects = list(roam.project.getProjects(projectpaths))
@@ -49,7 +35,7 @@ def excepthook(errorhandler, exctype, value, traceback):
     errorhandler(exctype, value, traceback)
 
 dialog = ConfigManagerDialog(projectpaths[0])
-sys.excepthook = partial(excepthook, dialog.raiseerror)
+sys.excepthook = functools.partial(excepthook, dialog.raiseerror)
 dialog.loadprojects(projects)
 dialog.show()
 #dialog.showMaximized()
