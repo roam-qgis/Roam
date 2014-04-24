8fd39f90e7890f73a26bd28e421a73566fa0e02b
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2014-04-24 22:13:59 +1000

    Move extra qgsexpression to new python module

--- src/roam/dataentrywidget.py
+++ src/roam/dataentrywidget.py
@@ -17,77 +17,8 @@ from roam.flickwidget import FlickCharm
 from roam.popupdialogs import DeleteFeatureDialog
 from roam.structs import CaseInsensitiveDict
 
-def qgsfunction(args, group, **kwargs):
-    """
-    Decorator function used to define a user expression function.
+import roam.qgisfunctions
 
-    Custom functions should take (values, feature, parent) as args,
-    they can also shortcut naming feature and parent args by using *args
-    if they are not needed in the function.
-
-    Functions should return a value compatible with QVariant
-
-    Eval errors can be raised using parent.setEvalErrorString()
-
-    Functions must be unregistered when no longer needed using
-    QgsExpression.unregisterFunction
-
-    Example:
-      @qgsfunction(2, 'test'):
-      def add(values, feature, parent):
-        pass
-
-      Will create and register a function in QgsExpression called 'add' in the
-      'test' group that takes two arguments.
-
-      or not using feature and parent:
-
-      @qgsfunction(2, 'test'):
-      def add(values, *args):
-        pass
-    """
-    helptemplate = ''
-    class QgsExpressionFunction(QgsExpression.Function):
-        def __init__(self, name, args, group, helptext=''):
-            QgsExpression.Function.__init__(self, name, args, group, helptext)
-
-        def func(self, values, feature, parent):
-            pass
-
-    def wrapper(func):
-        name = kwargs.get('name', func.__name__)
-        help = func.__doc__ or ''
-        help = help.strip()
-        if args == 0 and not name[0] == '$':
-            name = '${0}'.format(name)
-        func.__name__ = name
-        f = QgsExpressionFunction(name, args, group, '')
-        f.func = func
-        register = kwargs.get('register', True)
-        if register:
-            QgsExpression.registerFunction(f)
-        return f
-    return wrapper
-
-capturegeometry = None
-
-@qgsfunction(1, 'roam_geomvertex')
-def roam_geomvertex(values, feature, parent):
-    if capturegeometry:
-        nodeindex = values[0]
-        if capturegeometry.type() == QGis.Line:
-            line = capturegeometry.asPolyline()
-            try:
-                node = line[nodeindex]
-            except IndexError:
-                return None
-            node = QgsGeometry.fromPoint(node)
-            return node
-    return None
-
-@qgsfunction(0, 'roamgeometry')
-def _roamgeometry(values, feature, parent):
-    return capturegeometry
 
 class DefaultError(Exception):
     pass
@@ -135,8 +66,8 @@ def layer_value(feature, layer, field, defaultconfig, canvas):
     rect = canvas.mapRenderer().mapToLayerCoordinates(layer, rect)
     rq = QgsFeatureRequest().setFilterRect(rect)
     features = searchlayer.getFeatures(rq)
-    global capturegeometry
-    capturegeometry = feature.geometry()
+
+    roam.qgisfunctions = feature.geometry()
 
     exp = QgsExpression(expression)
     exp.prepare(searchlayer.pendingFields())
