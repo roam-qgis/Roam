19fbb09949925d37bcea329e1211b07e1a2b1233
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2014-05-16 16:49:36 +1000

    Remove prints from testing

--- tests/objects.py
+++ tests/objects.py
@@ -3,9 +3,6 @@ from qgis.core import QgsVectorLayer, QgsFeature, QgsProviderRegistry
 def newmemorylayer():
     uri = "point?crs=epsg:4326&field=id:integer"
     layer = QgsVectorLayer(uri, 'testlayer', "memory")
-    print QgsProviderRegistry.instance().pluginList()
-    print layer.dataProvider()
-    print layer.dataProvider().errors()
     return layer
 
 def addfeaturestolayer(layer, featurecount):
