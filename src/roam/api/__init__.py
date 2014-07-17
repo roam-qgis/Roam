19872668ec56dc16f27b37cbfaf6756bb3e387da
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2014-07-17 15:29:58 +1000

    Fix build error

--- src/roam/api/__init__.py
+++ src/roam/api/__init__.py
@@ -3,3 +3,5 @@ __author__ = 'Nathan.Woodrow'
 from roam.api.gps import GPS
 from roam.api.events import RoamEvents
 from roam.api.featureform import FeatureForm
+
+from roam.api.featureforms import inspectionform
