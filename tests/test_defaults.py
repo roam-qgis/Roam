e2430b67d14e9a1a2d1fd2484b822a1045010568
Nathan Woodrow <madmanwoo@gmail.com>
2014-11-24 20:31:44 +1000

    Fix failing default tests

--- tests/test_defaults.py
+++ tests/test_defaults.py
@@ -4,7 +4,10 @@ from qgis.core import QgsFeature, QgsFields, QgsField
 
 def test_variables_are_expanded():
     os.environ['TESTVAR'] = 'MYVAR'
-    default = "1 %TESTVAR% 2"
+    if os.name is 'nt':
+        default = "1 %TESTVAR% 2"
+    else:
+        default = "1 $TESTVAR 2"
     outdefault = roam.defaults.default_value(default, None, None)
     assert outdefault == '1 MYVAR 2'
     del os.environ['TESTVAR']
@@ -26,7 +29,10 @@ def test_replaced_with_qgsexpression_feature_attribute_lookup():
 
 def test_value_with_expression_and_variable_are_expanded():
     os.environ['TESTVAR'] = 'MYVAR'
-    default = "%TESTVAR% [% 'testvalue' %]"
+    if os.name is 'nt':
+        default = "%TESTVAR% [% 'testvalue' %]"
+    else:
+        default = "$TESTVAR [% 'testvalue' %]"
     outdefault = roam.defaults.default_value(default, None, None)
     assert outdefault == 'MYVAR testvalue'
     del os.environ['TESTVAR']
