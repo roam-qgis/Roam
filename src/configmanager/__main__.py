14d425d019ef89b90e1b348e1ebcbfa5396fe9b4
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2015-03-09 12:01:23 +1000

    Fix ECW loading in exe build

--- src/configmanager/__main__.py
+++ src/configmanager/__main__.py
@@ -5,6 +5,15 @@ import functools
 srcpath = os.path.dirname(os.path.realpath(sys.argv[0]))
 sys.path.append(srcpath)
 
+frozen = getattr(sys, "frozen", False)
+RUNNING_FROM_FILE = not frozen
+
+if frozen:
+    os.environ['PATH'] += ";{}".format(os.path.join(srcpath, 'libs'))
+    os.environ['PATH'] += ";{}".format(srcpath)
+    os.environ["GDAL_DRIVER_PATH"] = os.path.join(srcpath, 'libs')
+    os.environ["GDAL_DATA"] = os.path.join(srcpath, 'libs', 'gdal')
+
 import roam.environ
 
 with roam.environ.setup(logo=':/branding/config', title="IntraMaps Roam Config Manager") as roamapp:
