bab1ef62b03d69bcd3d8c82cdfa3f3d8fad41b63
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2016-01-22 17:02:49 +1000

    Fix loading of attachments. Load the attachment using desktop services if set

--- src/roam/editorwidgets/attachmentwidget.py
+++ src/roam/editorwidgets/attachmentwidget.py
@@ -1,6 +1,6 @@
 import os
-from PyQt4.QtCore import pyqtSignal
-from PyQt4.QtGui import QWidget, QAction, QIcon, QFileDialog
+from PyQt4.QtCore import pyqtSignal, QUrl
+from PyQt4.QtGui import QWidget, QAction, QIcon, QFileDialog, QPixmap, QDesktopServices
 
 from roam.editorwidgets.core import EditorWidget, registerwidgets
 from roam.editorwidgets.uifiles import ui_attachmentwidget
@@ -17,35 +17,44 @@ class RoamAttachmentWidget(ui_attachmentwidget.Ui_attachmentWidget, QWidget):
     def __init__(self, parent=None):
         super(RoamAttachmentWidget, self).__init__(parent)
         self.setupUi(self)
+        self._filename = None
+        self.selectbutton.setVisible(False)
+        self.deletebutton.setVisible(False)
 
         self.setStyleSheet(":hover {background-color: #dddddd;}")
         self.selectbutton.clicked.connect(self.attachmentloadrequest.emit)
         self.deletebutton.clicked.connect(self.removeAttachment)
         self.attachmentImage.mouseReleaseEvent = self.attach
-        self.removeAttachment()
-        self.filename = None
 
     def removeAttachment(self):
         self.attachmentremoved.emit()
-        self.isDefault = True
+        self.filename = None
 
     def attach(self, event):
         if self.isDefault:
             self.attachmentloadrequest.emit()
+        else:
+            QDesktopServices.openUrl(QUrl.fromLocalFile(self.filename))
 
     @property
     def isDefault(self):
         return self.filename is None
 
-    @isDefault.setter
-    def isDefault(self, value):
-        self.filename = None
-        self.selectbutton.setVisible(not value)
-        self.deletebutton.setVisible(not value)
+    @property
+    def filename(self):
+        return self._filename
+
+    @filename.setter
+    def filename(self, value):
+        self._filename = value
+        self.selectbutton.setVisible(value is not None)
+        self.deletebutton.setVisible(value is not None)
         if value:
             self.attachmentloaded.emit()
+            # TODO Add some generic file icons here.
         else:
             self.attachmentremoved.emit()
+            self.attachmentImage.setPixmap(QPixmap(":/widgets/add"))
 
 
 class AttachmentWidget(EditorWidget):
@@ -54,13 +63,13 @@ class AttachmentWidget(EditorWidget):
     def __init__(self, *args, **kwargs):
         super(AttachmentWidget, self).__init__(*args)
         self.defaultlocation = ''
-        self.savetofile = True
-        self.movetolocation = True
+        self.savelocation = ''
+        self.action = True
         self.modified = False
         self.filename = None
 
-        self.selectAction = QAction(QIcon(r":\widgets\folder"), "Add Attachment", None)
-        self.selectAction.triggered.connect(self._select_attachment)
+        # self.selectAction = QAction(QIcon(r":\widgets\folder"), "Add Attachment", None)
+        # self.selectAction.triggered.connect(self._select_attachment)
 
     def createWidget(self, parent):
         return RoamAttachmentWidget(parent)
@@ -68,7 +77,7 @@ class AttachmentWidget(EditorWidget):
     def initWidget(self, widget):
         widget.attachmentloaded.connect(self.emitvaluechanged)
         widget.attachmentremoved.connect(self.emitvaluechanged)
-        widget.attachmentloadrequest.connect(self.showpicker)
+        widget.attachmentloadrequest.connect(self._select_attachment)
 
     def showpicker(self):
         actionpicker = PickActionDialog(msg="Select attachment")
@@ -88,14 +97,23 @@ class AttachmentWidget(EditorWidget):
             return
 
         # TODO Move attachment to set folder.
-        name = os.path.basename(self.filename)
+        if self.action == "move":
+            print "Moving file"
+        elif self.action == "copy":
+            print "Copying file"
+
+        name = os.path.basename(attachment)
         self.filename = name
+        self.widget.filename = attachment
+
+        print "Attachment:", self.value()
 
         self.modified = True
 
     def updatefromconfig(self):
         self.defaultlocation = self.config.get('defaultlocation', '')
-        # self.movetolocation = self.config.get('copyormove', True)
+        self.action = self.config.get('action', 'copy')
+        self.savelocation = self.config.get('savelocation', '')
 
     def validate(self, *args):
         return self.filename is not None
