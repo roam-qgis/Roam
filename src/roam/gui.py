d18af6e86e3bdde12151d47ee3308d10ca26b9ee
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2016-02-02 07:16:51 +1000

    Clean up

--- src/roam/gui.py
+++ src/roam/gui.py
@@ -1,13 +1,6 @@
 from PyQt4.QtGui import QToolBar
 from PyQt4.QtCore import Qt
 
-from roam.legendwidget import LegendWidget
-from roam.gpswidget import GPSWidget
-from roam.syncwidget import SyncWidget
-from roam.settingswidget import SettingsWidget
-from roam.listmodulesdialog import ProjectsWidget
-from roam.mapwidget import MapWidget
-
 
 class HideableToolbar(QToolBar):
     def __init__(self, parent=None):
