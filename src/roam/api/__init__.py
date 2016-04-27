421fc57c7fbf6c27f2fc1d9512dbfa2ef79fa0fc
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2016-04-27 14:22:54 +1000

    Remove fake GPS for now

--- src/roam/api/__init__.py
+++ src/roam/api/__init__.py
@@ -5,10 +5,10 @@ GPS = gps.GPSService()
 
 import roam.config
 import os
-portname = roam.config.settings.get('gpsport', '')
-if os.path.exists(portname):
-    print "Setting fake GPS"
-    GPS = gps.FileGPSService(portname)
+# portname = roam.config.settings.get('gpsport', '')
+# if os.path.exists(portname):
+#     print "Setting fake GPS"
+#     GPS = gps.FileGPSService(portname)
 
 from roam.api.events import RoamEvents
 from roam.api.featureform import FeatureForm
