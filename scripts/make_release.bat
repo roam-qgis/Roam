567b71e4b4e127b2a2503c8df7034f6199d8a57d
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2016-05-04 10:20:33 +1000

    Don't package twice

--- scripts/make_release.bat
+++ scripts/make_release.bat
@@ -5,6 +5,9 @@ pushd %~dp0
 ECHO Creating release packages.
 SET QGISNAME=qgis
 CALL package.bat
+
+REM Don't let the make_installer script call package again
+SET DONTPACKAGE=1
 CALL make_installer.bat
 cd ..\dist
 rm ..\release -rf
