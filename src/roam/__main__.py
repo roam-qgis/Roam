5d4f6774d5fdf865073371b7f81c5c20e0bff0a1
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2016-12-09 14:11:32 +1000

    Fix loading of plugins from profile path

--- src/roam/__main__.py
+++ src/roam/__main__.py
@@ -45,7 +45,8 @@ with roam.environ.setup(srcpath) as roamapp:
     window.viewprojects()
     pluginpath = os.path.join(os.path.dirname(roamapp.settingspath), "plugins")
     apppluginpath = os.path.join(roamapp.apppath, "plugins")
+    profilepath = os.path.join(roamapp.profileroot, "plugins")
 
     import roam.api.plugins
-    roam.api.plugins.load_plugins_from([pluginpath, apppluginpath])
+    roam.api.plugins.load_plugins_from([pluginpath, apppluginpath, profilepath])
     window.show()
