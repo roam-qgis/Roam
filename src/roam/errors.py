ae4d8b51bc642f5254b53ec615635b3651d99e7b
Nathan.Woodrow <Nathan.woodrow@mapsolutions.com.au>
2017-06-09 10:48:24 +1000

    [Fix] Don't try to send errors if raven not found

--- src/roam/errors.py
+++ src/roam/errors.py
@@ -1,13 +1,18 @@
-from raven import Client
+"""
+Module to handle sending error reports.
+"""
 import roam
 import roam.config
 import roam.utils
 
+errorreporting = False
+try:
+    from raven import Client
+    errorreporting = True
+except ImportError:
+    errorreporting = False
+    roam.utils.warning("Error reporting disabled due to import error")
 
-client = Client(
-    dsn='http://681cb73fc39247d0bfa03437a9b53b61:114be99c3a8842188ae7e9381d30374a@sentry.kartoza.com/17',
-    release=roam.__version__
-    )
 
 
 def can_send():
@@ -19,7 +24,11 @@ def can_send():
 
 
 def send_exception(exinfo):
-    if can_send():
+    if can_send() and errorreporting:
+        client = Client(
+            dsn='http://681cb73fc39247d0bfa03437a9b53b61:114be99c3a8842188ae7e9381d30374a@sentry.kartoza.com/17',
+            release=roam.__version__
+        )
         roam.utils.info("Sending error report.")
         client.captureException(exinfo)
 
