0837f40752dbdaa54a937ae5370e2a4071f9129e
Nathan Woodrow <woodrow.nathan@gmail.com>
2017-09-11 08:48:32 +1000

    Add 2017 PyCharm

---
+++ scripts/editors/pycharm-pyqgis-2017.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+REM ---------------------------------------------------------------------------------------
+
+REM Script to setup the environment for building Roam with QGIS 2.x.
+REM This script sets the base folder that is used though out the build process
+REM and sets the location to Python.
+
+REM Change %OSGEO4W_ROOT% in setenv.bat to change in the location of QGIS.
+
+REM ---------------------------------------------------------------------------------------
+
+
+CALL ..\setenv.bat
+
+SET PYCHARM="C:\Program Files\JetBrains\PyCharm Community Edition 2017.2.2\bin\pycharm64.exe"
+
+start "PyCharm aware of QGIS" /B %PYCHARM% %*
