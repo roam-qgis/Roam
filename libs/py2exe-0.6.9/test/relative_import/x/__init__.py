3673c996f0bb134e2b7c205f740f1b6a407266b9
Nathan Woodrow <woodrow.nathan@gmail.com>
2017-09-11 13:57:08 +1000

    Pin py2exe in source

---
+++ libs/py2exe-0.6.9/test/relative_import/x/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+import sys as a
+from .. import sys as b
+
+def foo(): pass
---
+++ libs/py2exe-0.6.9/test/test_relative_import.py
@@ -0,0 +1,6 @@
+import relative_import.x
+import relative_import.x.module
+
+if __name__ == "__main__":
+    print relative_import.x.a.maxint
+    print relative_import.x.b.maxint
