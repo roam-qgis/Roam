8038bbcfdee291bd4236015d5f067ff34e54cd43
Nathan Woodrow <woodrow.nathan@gmail.com>
2018-08-20 10:53:50 +1000

    No VS needed anymore

--- scripts/setupdev.bat
+++ scripts/setupdev.bat
@@ -6,18 +6,11 @@ REM Change %OSGEO4W_ROOT% in setenv.bat to change in the install folder.
 
 REM ---------------------------------------------------------------------------------------
 
-IF NOT EXIST "%VS90COMNTOOLS%" GOTO NOVS
-
 pushd %~dp0
 CALL setenv.bat %1
 CALL "%OSGEO4W_ROOT%\bin\o4w_env.bat"
 
 python -m pip install -r ..\requirements.txt
-GOTO END
-
-:NOVS
-ECHO You don't seem to have Visual Studio C++ 2008 installed.
-ECHO We need that to build py2exe
 GOTO END
 
 :END
