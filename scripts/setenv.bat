2b50e793402cc6e8969dc3411a7e7f9bfd9bd617
Terry McDonell <terry@longmacch.com.au>
2019-08-09 08:49:17 +1000

    Improve path variable selection and update for Python 3.7

--- scripts/setenv.bat
+++ scripts/setenv.bat
@@ -9,18 +9,27 @@ REM ----------------------------------------------------------------------------
 
 for %%x in (%cmdcmdline%) do if /i "%%~x"=="/c" set DOUBLECLICKED=1
 
-FOR /D %%p in ("C:\Program Files\QGIS 3.*","C:\Program Files (x86)\QGIS 3.*") DO (
-    SET OSGEO4W_ROOT=%%p
+SET OSGEO4W_ROOT=
+FOR /D %%p in ("%PROGRAMFILES%\QGIS 3.*","%PROGRAMFILES(X86)%\QGIS 3.*") DO (
+    SET OSGEO4W_ROOT="%%~fsp"
 )
 
 REM Change OSGeo4W_ROOT here to point to your install of QGIS if it is not in the standard directories.
 IF NOT DEFINED OSGEO4W_ROOT SET OSGEO4W_ROOT=C:\OSGeo4W
+SET OSGEO4W_ROOT=%OSGEO4W_ROOT:"=%
 
-IF EXIST %OSGEO4W_ROOT%\bin\qgis-ltr.bat (
+IF EXIST "%OSGEO4W_ROOT%\bin\qgis-ltr.bat" (
     SET QGISNAME=qgis-ltr
 ) ELSE (
     SET QGISNAME=qgis
 )
+
+IF EXIST "%PROGRAMFILES%\Git\bin" (
+    SET GIT="%PROGRAMFILES%\Git"
+) ELSE (
+    SET GIT="%PROGRAMFILES(X86)%\Git"
+)
+SET GIT=%GIT:"=%
 
 ECHO Setup environment for
 ECHO ROOT: %OSGEO4W_ROOT%
@@ -28,16 +37,16 @@ ECHO QGIS: %QGISNAME%
 SET QGIS=%OSGEO4W_ROOT%\apps\%QGISNAME%
 set QGIS_PREFIX_PATH=%QGIS%
 
-CALL "%OSGEO4W_ROOT%\bin\o4w_env.bat"
+SET GDAL_DATA=%OSGEO4W_ROOT%\share\gdal
+SET GDAL_DRIVER_PATH=%OSGEO4W_ROOT%\bin\gdalplugins
+SET PROJ_LIB=%OSGEO4W_ROOT%\share\proj
+
+set PATH=%OSGEO4W_ROOT%\bin;%QGIS%\bin;%BASE%\src\;%GIT%\bin;;%WINDIR%\system32;%WINDIR%;%WINDIR%\system32\WBem
 
 : Python Setup
-SET BASE=%~dp0..\
-set PATH=%OSGEO4W_ROOT%\bin;%QGIS%\bin;%BASE%\src\;C:\Program Files (x86)\Git\bin;%PATH%;
-SET PYTHONHOME=%OSGEO4W_ROOT%\apps\Python27
-set PYTHONPATH=%BASE%\src;%QGIS%\python;%BASE%\libs
+CALL %OSGEO4W_ROOT%\bin\py3_env.bat
 
 ECHO OSGeo path is: %OSGEO4W_ROOT%
 ECHO Getting QGIS libs from: %QGIS%
 ECHO Python loaded from: %PYTHONHOME%
 ECHO PATH is: %PATH%
-ECHO Python libs from: %PYTHONPATH%
