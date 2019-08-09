77b919a5f965428b8087afb0f2cb96a67e2e77a6
Terry McDonell <terry@longmacch.com.au>
2019-08-09 12:38:18 +1000

    Use environment variables to define QGIS path
    Not sure if these variables are carried through - may have to revert later.

--- src/configmanager/utils.py
+++ src/configmanager/utils.py
@@ -20,34 +20,11 @@ def openqgis(project):
     :param project:
     :return:
     """
-    programfiles = os.environ["ProgramFiles"]
-    programfilesW6432 = os.environ["ProgramW6432"]
-    def checkpath(programfiles, version, name="qgis"):
-        qgispath = os.path.join(programfiles, version, "bin", "{}.bat".format(name))
-        return os.path.exists(qgispath), qgispath
-
-    if "2.16" in QGis.QGIS_VERSION:
-        # Try 32 bit first so we can match Roam version even though it doesn't really
-        # matter
-        found, path = checkpath(programfiles, "QGIS 2.16")
-        if found:
-            qgislocation = path
-        else:
-            found, path = checkpath(programfilesW6432, "QGIS 2.16")
-            if found:
-                qgislocation = path
-    elif "2.18" in QGis.QGIS_VERSION:
-        # Try 32 bit first so we can match Roam version even though it doesn't really
-        # matter
-        found, path = checkpath(programfiles, "QGIS 2.18", "qgis-ltr")
-        if found:
-            qgislocation = path
-        else:
-            found, path = checkpath(programfilesW6432, "QGIS 2.18", "qgis-ltr")
-            if found:
-                qgislocation = path
-    else:
-        qgislocation = r'C:\OSGeo4W\bin\qgis.bat'
+    osgeo4w = os.environ["OSGEO4W_ROOT"]
+    name = os.environ["QGISNAME"]
+    qgislocation = os.path.join(osgeo4w, "bin", "{}.bat".format(name))
+    
+    if not roam.config.settings.get("qgislocation", qgislocation) and os.path.exists(qgislocation):
         qgislocation = roam.config.settings.setdefault('configmanager', {}) \
             .setdefault('qgislocation', qgislocation)
 
