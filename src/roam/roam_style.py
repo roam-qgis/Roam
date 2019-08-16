68409b43f404034a56df83474000f9787873cc29
Terry McDonell <terry@longmacch.com.au>
2019-08-16 15:15:17 +1000

    Initial attempt to make calendar more readable & user-friendly on a tablet.

--- src/roam/roam_style.py
+++ src/roam/roam_style.py
@@ -182,6 +182,77 @@ def appstyle():
         padding: 6px;
         color: #4f4f4f;
     }
+    
+    QCalendarWidget QWidget#qt_calendar_navigationbar { 
+        background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop: 0 #cccccc, stop: 1 #333333); 
+    }
+
+    QCalendarWidget QToolButton {
+        height: 60px;
+        width: 150px;
+        color: white;
+        font-size: 24px;
+        icon-size: 56px, 56px;
+        background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop: 0 #cccccc, stop: 1 #333333);
+    }
+
+    QCalendarWidget QMenu {
+        width: 150px;
+        left: 20px;
+        color: white;
+        font-size: 18px;
+        background-color: rgb(100, 100, 100);
+    }
+
+    QCalendarWidget QSpinBox { 
+        width: 150px; 
+        font-size:24px; 
+        color: white; 
+        background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop: 0 #cccccc, stop: 1 #333333); 
+        selection-background-color: rgb(136, 136, 136);
+        selection-color: rgb(255, 255, 255);
+    }
+
+    QCalendarWidget QSpinBox::up-button { 
+        subcontrol-origin: border;  
+        subcontrol-position: top right;  
+        width:65px; 
+    }
+
+    QCalendarWidget QSpinBox::down-button {
+        subcontrol-origin: border; 
+        subcontrol-position: bottom right;  
+        width:65px;
+    }
+
+    QCalendarWidget QSpinBox::up-arrow { 
+        width:56px;  
+        height:56px; 
+    }
+
+    QCalendarWidget QSpinBox::down-arrow {
+        width:56px;  
+        height:56px; 
+    }
+    
+    /* header row */
+    QCalendarWidget QWidget {
+        alternate-background-color: rgb(128, 128, 128);
+    }
+    
+    /* normal days */
+    QCalendarWidget QAbstractItemView:enabled {
+        font-size:24px;  
+        color: rgb(180, 180, 180);  
+        background-color: black;  
+        selection-background-color: rgb(64, 64, 64); 
+        selection-color: rgb(0, 255, 0); 
+    }
+    
+    /* days in other months */
+    QCalendarWidget QAbstractItemView:disabled { 
+        color: rgb(64, 64, 64);
+    }
 
     QWidget#featureformarea {
         background-color: white;
