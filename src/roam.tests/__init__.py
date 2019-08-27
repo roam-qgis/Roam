7762099bee949915e8b076424e36a4a3e20cd6a1
Nathan Woodrow <nathan.woodrow@chartistechnology.com>
2019-08-27 13:04:56 +1000

    Move test path setup

--- src/roam.tests/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-# Add parent directory to path to make test aware of other modules
-srcfolder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', "src"))
-if srcfolder not in sys.path:
-    sys.path.append(srcfolder)
-
-
--- src/roam.tests/conftest.py
+++ src/roam.tests/conftest.py
@@ -1,11 +1,18 @@
 import os
 import pytest
 import sys
+
+# Add parent directory to path to make test aware of other modules
+srcfolder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if srcfolder not in sys.path:
+    sys.path.insert(0, srcfolder)
+
 import roam.environ
 
 print(sys.argv)
 config = {}
 roamapp = roam.environ._setup(os.getcwd(), config=config)
+
 
 def tear_down():
     roamapp.exit()
