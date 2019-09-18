7757892d60e322236e4a6cb3bc9dfb175f5d819b
Nathan Woodrow <nathan.woodrow@chartistechnology.com>
2019-09-18 15:44:08 +1000

    Fix finger scroll on help viewer

--- src/roam/helpviewdialog.py
+++ src/roam/helpviewdialog.py
@@ -1,7 +1,8 @@
-from qgis.PyQt.QtWidgets import QDialog
-from qgis.PyQt.QtCore import QUrl, QEvent, Qt
-from qgis.PyQt.QtWebKitWidgets import QWebPage
+from qgis.PyQt.QtWidgets import QDialog, QScrollArea, QGraphicsScene, QGraphicsView
+from qgis.PyQt.QtCore import QUrl, QEvent, Qt, QSize
+from qgis.PyQt.QtWebKitWidgets import QWebPage, QGraphicsWebView
 
+from roam.api.utils import install_touch_scroll
 from roam.api.events import RoamEvents
 from roam.ui.uifiles import (helpviewer_widget, helpviewer_base,
                              helppage_widget, helppage_base)
@@ -24,8 +25,18 @@ class HelpPage(helppage_widget, helppage_base):
         if self.parent():
             self.parent().installEventFilter(self)
 
+        self.view = QGraphicsView()
+        self.webView = QGraphicsWebView()
+        self.scene = QGraphicsScene()
+        self.webView.setResizesToContents(True)
+        self.view.setScene(self.scene)
+        self.scene.addItem(self.webView)
+
         self.webView.linkClicked.connect(RoamEvents.openurl.emit)
         self.webView.page().setLinkDelegationPolicy(QWebPage.DelegateAllLinks)
+
+        install_touch_scroll(self.view)
+        self.layout().addWidget(self.view)
 
     def eventFilter(self, object, event):
         if event.type() == QEvent.Resize:
